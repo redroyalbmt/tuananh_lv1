a0ba9e3b4573615b4bfb20e14b28b859e0ac7079
Tuan anh <143373494+redroyalbmt@users.noreply.github.com>
2023-09-12 00:18:00 +0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
-    login();
+    login();getData();
 });
+const url= "https://students.trungthanhweb.com/api/";
+const urlimage= "https://students.trungthanhweb.com/images/";
+
 const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
     showConfirmButton: false,
-    timer: 3000,
+    timer: 1700,
     timerProgressBar: true,
     didOpen: (toast) => {
       toast.addEventListener('mouseenter', Swal.stopTimer)
       toast.addEventListener('mouseleave', Swal.resumeTimer)
     }
   })
+
 function login(){
     $("#loginbtn").click(function (e) { 
         e.preventDefault();
@@ -52,5 +56,60 @@ function login(){
             }
           
         });
+    });
+}
+function getData(){
+    
+    $.ajax({
+        type: "GET",
+        url: url+"home",
+        // tuong duong nhu url
+        data: {
+            apitoken:localStorage.getItem('token')
+        },
+        dataType: "JSON",
+        success: function (res) {
+            console.log(res.products.data);
+            if(res.categrories.length>0){
+                var str=``;
+                res.categrories.forEach(el => {
+                    str+=`
+                    <li><a class="dropdown-item" href="#">`+el.name+`</a></li>
+  
+                    `
+                });
+                $("#loaispUL").html(str);
+                str=``;
+                res.brands.forEach(el => {
+                    str+=`
+                    <li><a class="dropdown-item" href="#">`+el.name+`</a></li>
+  
+                    `
+                });
+                $("#brandsUL").html(str);
+            }
+            if(res.products.data.length>0){
+                var str=``;
+                res.products.data.forEach(el => {
+                    str+=`
+                    <div class="col-md-3">
+                    <div class="card" style="width: 18rem;">
+                      <img src="`+urlimage+el.images+`" class="card-img-top" alt="...">
+                      <div class="card-body productinfo">
+                        <h5 class="card-title">`+el.name+`</h5>
+                        <span class="card-text">Tên Sản Phẩm: `+el.name+`</span><br>
+                        <span class="card-text">Giá :`+Intl.NumberFormat('en-US').format(Number(el.price))+`</span><br>
+                        <span class="card-text">Thương Hiệu :`+el.brandname+`</span><br><br>
+                        <a href="#" class="btn btn-primary">Xem Thêm</a>
+                        <a href="#" class="btn btn-warning ms-3">Đặt Hàng</a>
+                      </div>
+                    </div>
+                  </div>
+
+                    `;
+                });
+                $("#productfetch").html(str);
+            }
+        }
     });
 }
